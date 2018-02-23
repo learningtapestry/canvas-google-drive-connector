4576cc9e1c5550a7d139131ea32eed07726b75af
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2018-02-23 16:58:29 -0300

    (#5) Embed gdrive files on the editor

--- lib/content_items.rb
+++ lib/content_items.rb
@@ -1,25 +1,23 @@
-module ContentItems
-  def self.build_link(f)
-    {
-      "@context" => 'http://purl.imsglobal.org/ctx/lti/v1/ContentItem',
-      "@graph" => [
-        {
-          "@type": "ContentItem",
-          "@id": f.id,
-          "url": f.link,
-          "title": f.name,
-          "text": f.name,
-          "mediaType": "text/html",
-        }
-      ]
-    }
-  end
+# frozen_string_literal: true
 
-  def self.build_embed(f)
-    {
-      "@context" => 'http://purl.imsglobal.org/ctx/lti/v1/ContentItem',
-      "@graph" => [
-      ]
+module ContentItems
+  def self.build(f, type)
+    item = {
+      '@type': 'ContentItem',
+      '@id': f.id,
+      url: f.link,
+      title: f.name,
+      mediaType: 'text/html',
     }
+    if type == :embed
+      item[:placementAdvice] = {
+        presentationDocumentTarget: 'iframe',
+        displayWidth: '100%',
+        displayHeight: '600'
+      }
+    else # :link
+      item[:text] = f.name
+    end
+    { '@context': 'http://purl.imsglobal.org/ctx/lti/v1/ContentItem', '@graph': [item] }
   end
 end
