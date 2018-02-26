cafb752f03bbc26554d8cf2a2f0598d1b02c027b
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2018-02-26 13:25:15 -0300

    Add a few more tests

--- tests/app_test.rb
+++ tests/app_test.rb
@@ -6,9 +6,30 @@ def app
   Sinatra::Application
 end
 
-describe :app do
-  it 'have a valid root endpoint' do
-    get '/'
-    assert last_response.ok?
+describe 'root' do
+  before { get '/' }
+
+  it { assert last_response.ok? }
+end
+
+describe 'config' do
+  before { get '/config.xml' }
+
+  it { assert last_response.ok? }
+  it { assert last_response.content_type.match(%r{application/xml}) }
+  it { assert last_response.match(/<blti:launch_url>/) }
+end
+
+describe 'credentials' do
+  it 'renders a generate credentials buttons' do
+    get '/credentials/new'
+    assert last_response.match(/class="credentials-generate-btn"/)
+  end
+
+  it 'generates new credentials' do
+    creds_count = AuthCredential.count
+    post '/credentials'
+    assert_equal AuthCredential.count, creds_count + 1
+    assert last_response.match(%r{<div class="credential-value">#{AuthCredential.last.key}</div>})
   end
 end
